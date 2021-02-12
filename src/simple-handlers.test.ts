09f7bb478cadb765ba2fa3167580d9842f131dfb
Jared Forsyth <jared@jaredforsyth.com>
2021-02-12 14:35:33 -0600

    recursive handler

--- src/simple-handlers.test.ts
+++ src/simple-handlers.test.ts
@@ -28,6 +28,33 @@ const handleSimple = <Get, R>(
     );
 };
 
+describe('handleSimple', () => {
+    it('should work', () => {
+        const x = (currentHandler, done) => z(currentHandler, (r) => done(r));
+        const z = (currentHandler, done) => currentHandler((v) => done(v + 2));
+        const m = (_currentHandler, done) => done(21);
+
+        const myHandler = (fn, done) => {
+            handleSimple(
+                fn,
+                (resumeExecution) =>
+                    resumeExecution(5, (finalExecutionValue) =>
+                        done([finalExecutionValue, 'handled']),
+                    ),
+                (pureValue) => done([pureValue, 'pure']),
+            );
+        };
+
+        const k = jest.fn();
+        myHandler(m, k);
+        expect(k).toBeCalledWith([21, 'pure']);
+
+        const k2 = jest.fn();
+        myHandler(x, k2);
+        expect(k2).toBeCalledWith([7, 'handled']);
+    });
+});
+
 // This is a function that allows you to handle
 // effects of type `{ eff () -> R }`
 const handleBidirectional = <Get, Set, R>(
@@ -56,33 +83,8 @@ const handleBidirectional = <Get, Set, R>(
     );
 };
 
-describe('ok', () => {
-    it('simple "get" handler', () => {
-        const x = (currentHandler, done) => z(currentHandler, (r) => done(r));
-        const z = (currentHandler, done) => currentHandler((v) => done(v + 2));
-        const m = (_currentHandler, done) => done(21);
-
-        const myHandler = (fn, done) => {
-            handleSimple(
-                fn,
-                (resumeExecution) =>
-                    resumeExecution(5, (finalExecutionValue) =>
-                        done([finalExecutionValue, 'handled']),
-                    ),
-                (pureValue) => done([pureValue, 'pure']),
-            );
-        };
-
-        const k = jest.fn();
-        myHandler(m, k);
-        expect(k).toBeCalledWith([21, 'pure']);
-
-        const k2 = jest.fn();
-        myHandler(x, k2);
-        expect(k2).toBeCalledWith([7, 'handled']);
-    });
-
-    it('bidirectional, love it', () => {
+describe('handleBidirectional', () => {
+    it('should work', () => {
         const x = (currentHandler, done) => z(currentHandler, (r) => done(r));
         const z = (currentHandler, done) =>
             currentHandler(42, (v) => done([...v, 'z done']));
@@ -109,39 +111,86 @@ describe('ok', () => {
         myTimes5(x, k2);
         expect(k2).toBeCalledWith([210, 'sent', 'z done', 'handled']);
     });
+});
 
-    // it('handle multiple by recursion', () => {
-    //     const x = (h, k) => y(h, (r) => k(r));
-    //     const y = (h, k) => z(h, (r) => k(r));
-    //     const z = (h, k) =>
-    //         // first get
-    //         h((v) => {
-    //             m(h, (r) =>
-    //                 // second get
-    //                 h((v2) => k(v + v2 + r)),
-    //             );
-    //         });
-    //     const m = (h, k) => k(21);
+type ShallowHandler<Get> = (
+    returnIntoFn: (newHandler: ShallowHandler<Get>, value: Get) => void,
+) => void;
 
-    //     const h = (n) => (x, k) => {
-    //         handleSimple(
-    //             x,
-    //             // first handle
-    //             (ki) =>
-    //                 /*
-    //             (get () -> k) =>
-    //                 handle [(k n), m, 4] with h(n * 10)
-    //             */
-    //                 h(n * 10)((h, k) => ki(n, (r) => k([r, n, 'handled'])), k),
-    //             (a) => k([a, 'pure', n]),
-    //         );
-    //     };
+// This is a function that allows you to handle
+// effects of type `{ eff () -> Get }` and a function
+// of type `() -> R` (written in direct, non-CPS style)
+const handleSimpleShallow = <Get, R>(
+    fn: (handler: ShallowHandler<Get>, cb: (fnReturnValue: R) => void) => void,
+    handleEffect: (
+        cb: (
+            gotten: Get,
+            newHandler: ShallowHandler<Get>,
+            returnIntoHandler: (fnReturnValue: R) => void,
+        ) => void,
+    ) => void,
+    handlePure: (fnReturnValue: R) => void,
+) => {
+    let fnsReturnPointer = handlePure;
+    fn(
+        (returnIntoFn) => {
+            handleEffect(
+                (handlersValueToSend, newHandler, returnIntoHandler) => {
+                    fnsReturnPointer = returnIntoHandler;
+                    returnIntoFn(newHandler, handlersValueToSend);
+                },
+            );
+        },
+        (fnsReturnValue) => fnsReturnPointer(fnsReturnValue),
+    );
+};
 
-    //     const k2 = jest.fn();
-    //     h(5)(x, k2);
-    //     expect(k2).toBeCalledTimes(1);
-    //     expect(k2).toBeCalledWith([28, 4]);
-    // });
+describe('handleSimpleShallow', () => {
+    it('should work', () => {
+        const x = (currentHandler, done) => z(currentHandler, (r) => done(r));
+        const z = (currentHandler, done) =>
+            currentHandler((newHandler, one) =>
+                newHandler((_newNewHandler, two) =>
+                    done(['one', one, 'two', two]),
+                ),
+            );
+
+        const respondWith = (responseValue: number) => (
+            fn: (
+                currentHandler: ShallowHandler<number>,
+                cb: (value: Array<any>) => void,
+            ) => void,
+            done: (res: Array<any>) => void,
+        ) => {
+            handleSimpleShallow(
+                fn,
+                (resumeExecution) =>
+                    respondWith(responseValue + 5)(
+                        (newHandler, innerDone) =>
+                            resumeExecution(
+                                responseValue,
+                                newHandler,
+                                (finalExecutionValue) =>
+                                    innerDone([
+                                        ...finalExecutionValue,
+                                        'handled',
+                                        responseValue,
+                                    ]),
+                            ),
+                        done,
+                    ),
+                (pureValue) => done([pureValue, 'pure', responseValue]),
+            );
+        };
+
+        const k = jest.fn();
+        respondWith(10)(x, k);
+        expect(k).toBeCalledWith([
+            ['one', 10, 'two', 15, 'handled', 10, 'handled', 15],
+            'pure',
+            20,
+        ]);
+    });
 
     it('double stack', () => {
         // print and get maybe
