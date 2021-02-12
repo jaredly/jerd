b2a86f99c403cbee65f57b7065092c6cb3bd77a9
Jared Forsyth <jared@jaredforsyth.com>
2021-02-11 20:47:30 -0600

    ok that at least gets us the overriding behavior that I want

--- js-handlers.js
+++ js-handlers.js
@@ -1,15 +1,15 @@
 // umm ok
 
 const choose = (a, b, handlers, k) => {
-    return handlers.decide((v) => k(v ? a : b));
+    return handlers.decide((handlers, v) => k(handlers, v ? a : b));
 };
 
 const chooseDiff = (handlers, k) => {
-    return choose(15, 30, handlers, (x) => {
-        console.log('first choose', x);
-        return choose(5, 10, handlers, (y) => {
-            console.log('second choose', x, y);
-            return k(x - y);
+    return choose(15, 30, handlers, (handlers, x) => {
+        console.log('first choose', x, handlers.i);
+        return choose(5, 10, handlers, (handlers, y) => {
+            console.log('second choose', x, y, handlers.i);
+            return k(handlers, x - y);
         });
     });
 };
@@ -17,16 +17,24 @@ const chooseDiff = (handlers, k) => {
 const pickMax = (i, f) => {
     return f(
         {
+            i,
             decide: (k) => {
-                const t = pickMax(i + 10, (handlers, k2) => k2(k(true)));
+                const t = pickMax(i + 10, (handlers, k2) =>
+                    k2(handlers, k(handlers, true)),
+                );
                 console.log(i, 't', t);
-                const f = pickMax(i + 11, (handlers, k2) => k2(k(false)));
+                const f = pickMax(i + 11, (handlers, k2) =>
+                    k2(handlers, k(handlers, false)),
+                );
                 console.log(i, 'f', f);
                 console.log(i, 'checking', t, f);
                 return Math.max(t, f);
             },
         },
-        (x) => x,
+        (_, x) => {
+            console.log('pure', i, x);
+            return x;
+        },
     );
 };
 
