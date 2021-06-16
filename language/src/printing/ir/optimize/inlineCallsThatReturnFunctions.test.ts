d12a3408a836e5833b2475dd8e3474d8dfd9cca3
Jared Forsyth <jared@jaredforsyth.com>
2021-06-16 09:27:25 -0500

    nice

--- language/src/printing/ir/optimize/inlineCallsThatReturnFunctions.test.ts
+++ language/src/printing/ir/optimize/inlineCallsThatReturnFunctions.test.ts
@@ -3,15 +3,15 @@ import { runFixture, snapshotSerializer } from './optimizeTestUtils';
 
 expect.addSnapshotSerializer(snapshotSerializer);
 
-describe('inlineCallsThatReturnFunctions', () => {
+describe('serializer', () => {
     it('should work', () => {
-        expect(runFixture(`2 + 3`, inlineCallsThatReturnFunctions))
-            .toMatchInlineSnapshot(`
-              // 23157700
-              +#builtin(2, 3)
-        `);
+        expect(
+            runFixture(`2 + 3`, (_, __, ___, expr) => expr),
+        ).toMatchInlineSnapshot(`  const expr0#23157700 = +#builtin(2, 3)`);
     });
+});
 
+describe('inlineCallsThatReturnFunctions', () => {
     it('should inline a function call.', () => {
         expect(
             runFixture(
@@ -22,11 +22,27 @@ describe('inlineCallsThatReturnFunctions', () => {
                 inlineCallsThatReturnFunctions,
             ),
         ).toMatchInlineSnapshot(`
-              // 7c2fb0d1
-              ((n#:1: string#builtin) => (m#:2: string#builtin) => +#builtin(
-                  m#:2,
-                  n#:1,
-              ))("hi")("ho")
+              const expr0#7c2fb0d1 = ((n#:1: string#builtin) => (
+                  m#:2: string#builtin,
+              ) => +#builtin(m#:2, n#:1))("hi")("ho")
+        `);
+    });
+
+    it('should not inline a function call that doesnt return a function.', () => {
+        expect(
+            runFixture(
+                `
+		const f = (n: string) => n + "hi"
+		f("ho")
+		`,
+                inlineCallsThatReturnFunctions,
+            ),
+        ).toMatchInlineSnapshot(`
+              const f#48f8d2f4 = (n#:0: string#builtin) => +#builtin(
+                  n#:0,
+                  "hi",
+              )
+              const expr0#16f60da6 = f#48f8d2f4("ho")
         `);
     });
 });
---
+++ language/src/printing/ir/optimize/optimizeTestUtils.ts
@@ -0,0 +1,72 @@
+import { parse } from '../../../parsing/grammar';
+import { hashObject, idFromName, idName } from '../../../typing/env';
+import { typeFile } from '../../../typing/typeFile';
+import { Env, newWithGlobal, nullLocation, Term } from '../../../typing/types';
+import { assembleItemsForFile } from '../../glslPrinter';
+import { debugExpr, idToDebug } from '../../irDebugPrinter';
+import { loadInit } from '../../loadPrelude';
+import { atom, items, printToString } from '../../printer';
+import { Exprs, Optimizer } from './optimize';
+
+const init = loadInit();
+
+export const runFixture = (text: string, optimize: Optimizer) => {
+    const initialEnv = newWithGlobal(init.initialEnv);
+    const { env, expressions } = typeFile(
+        parse(text),
+        initialEnv,
+        'test-fixture',
+    );
+
+    const mains: Array<Term> = expressions.map((expr, i) => {
+        const hash = hashObject(expr);
+        const id = { hash, size: 1, pos: 0 };
+        env.global.idNames[idName(id)] = 'expr' + i;
+        env.global.terms[hash] = expr;
+        return {
+            type: 'ref',
+            ref: { type: 'user', id },
+            location: nullLocation,
+            is: expr.is,
+        };
+    });
+
+    const { inOrder, irTerms } = assembleItemsForFile(
+        env,
+        mains,
+        mains.map((main) => idName((main as any).ref.id)),
+        {},
+        {},
+        optimize,
+    );
+
+    return { env, irTerms, inOrder };
+};
+
+export const snapshotSerializer: jest.SnapshotSerializerPlugin = {
+    test(value) {
+        return value && value.env && value.irTerms && value.inOrder != null;
+    },
+    print(value, _, indent) {
+        const { env, inOrder, irTerms } = value as {
+            env: Env;
+            inOrder: Array<string>;
+            irTerms: Exprs;
+        };
+        return indent(
+            inOrder
+                .map((id) =>
+                    printToString(
+                        items([
+                            atom('const '),
+                            idToDebug(env, idFromName(id), false),
+                            atom(' = '),
+                            debugExpr(env, irTerms[id].expr),
+                        ]),
+                        50,
+                    ),
+                )
+                .join('\n'),
+        );
+    },
+};
