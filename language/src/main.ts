de4fc2620f93a6ee5f94de80284f375c7d53cef0
Jared Forsyth <jared@jaredforsyth.com>
2021-07-27 07:36:40 -0600

    refactor main.ts a little

--- language/src/main.ts
+++ language/src/main.ts
@@ -449,30 +449,72 @@ const watchFiles = (files: Array<string>, fn: () => void) => {
     fn();
 };
 
-if (process.argv[2] === 'go') {
-    console.log('go please');
-    const fnames = process.argv
-        .slice(3)
-        .filter((name) => !name.startsWith('-'));
-    const assert = process.argv.includes('--assert');
-    const run = process.argv.includes('--run');
-    mainGo(expandDirectories(fnames), assert, run);
-} else if (process.argv[2] === 'watch') {
-    // This is just for recompiling something
-    const fnames = process.argv
-        .slice(3)
-        .filter((name) => !name.startsWith('-'));
-    const assert = process.argv.includes('--assert');
-    const run = process.argv.includes('--run');
-    const cache =
-        process.argv.includes('--cache') &&
-        !process.argv.includes('--no-cache');
-    const failFast = process.argv.includes('--fail-fast');
-    const glsl = process.argv.includes('--glsl');
-    const trace = process.argv.includes('--trace');
+const commands: { [key: string]: (args: Array<string>) => void } = {
+    go: (args) => {
+        console.log('go please');
+        const fnames = args.filter((name) => !name.startsWith('-'));
+        const assert = args.includes('--assert');
+        const run = args.includes('--run');
+        mainGo(expandDirectories(fnames), assert, run);
+    },
+    watch: (args) => {
+        // This is just for recompiling something
+        const fnames = args.filter((name) => !name.startsWith('-'));
+        const assert = args.includes('--assert');
+        const run = args.includes('--run');
+        const cache = args.includes('--cache') && !args.includes('--no-cache');
+        const failFast = args.includes('--fail-fast');
+        const glsl = args.includes('--glsl');
+        const trace = args.includes('--trace');
 
-    const init = loadInit();
-    watchFiles(fnames, () => {
+        const init = loadInit();
+        watchFiles(fnames, () => {
+            try {
+                const failed = main(
+                    expandDirectories(fnames),
+                    {
+                        assert,
+                        run,
+                        cache,
+                        failFast,
+                        glsl,
+                        trace,
+                    },
+                    init,
+                );
+                if (failed) {
+                    console.log('Failed');
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        });
+    },
+    '--test': () => runTests(),
+    pretty: (args) => {
+        const fnames = args.filter((name) => !name.startsWith('-'));
+        const init = loadInit();
+        const initialEnv = newWithGlobal(init.initialEnv);
+        fnames.forEach((fname) => {
+            const raw = fs.readFileSync(fname, 'utf8');
+            const parsed: Array<Toplevel> = parse(raw);
+
+            const { expressions, env } = typeFile(parsed, initialEnv, fname);
+            expressions.forEach((term) => {
+                const text = printToString(termToPretty(env, term), 50);
+                console.log(text);
+            });
+            console.log('ok');
+        });
+    },
+    [':fallback:']: (args) => {
+        const fnames = args.filter((name) => !name.startsWith('-'));
+        const assert = args.includes('--assert');
+        const run = args.includes('--run');
+        const cache = args.includes('--cache') && !args.includes('--no-cache');
+        const failFast = args.includes('--fail-fast');
+        const glsl = args.includes('--glsl');
+        const trace = args.includes('--trace');
         try {
             const failed = main(
                 expandDirectories(fnames),
@@ -484,64 +526,21 @@ if (process.argv[2] === 'go') {
                     glsl,
                     trace,
                 },
-                init,
+                loadInit(),
             );
             if (failed) {
-                console.log('Failed');
+                process.exit(1);
             }
         } catch (err) {
             console.error(err);
-        }
-    });
-} else if (process.argv[2] === '--test') {
-    runTests();
-} else if (process.argv[2] === 'pretty') {
-    const fnames = process.argv
-        .slice(3)
-        .filter((name) => !name.startsWith('-'));
-    const init = loadInit();
-    const initialEnv = newWithGlobal(init.initialEnv);
-    fnames.forEach((fname) => {
-        const raw = fs.readFileSync(fname, 'utf8');
-        const parsed: Array<Toplevel> = parse(raw);
-
-        const { expressions, env } = typeFile(parsed, initialEnv, fname);
-        expressions.forEach((term) => {
-            const text = printToString(termToPretty(env, term), 50);
-            console.log(text);
-        });
-        console.log('ok');
-    });
-} else {
-    const fnames = process.argv
-        .slice(2)
-        .filter((name) => !name.startsWith('-'));
-    const assert = process.argv.includes('--assert');
-    const run = process.argv.includes('--run');
-    const cache =
-        process.argv.includes('--cache') &&
-        !process.argv.includes('--no-cache');
-    const failFast = process.argv.includes('--fail-fast');
-    const glsl = process.argv.includes('--glsl');
-    const trace = process.argv.includes('--trace');
-    try {
-        const failed = main(
-            expandDirectories(fnames),
-            {
-                assert,
-                run,
-                cache,
-                failFast,
-                glsl,
-                trace,
-            },
-            loadInit(),
-        );
-        if (failed) {
             process.exit(1);
         }
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
+    },
+};
+
+const command = process.argv[2];
+if (commands[command]) {
+    commands[command](process.argv.slice(3));
+} else {
+    commands[':fallback:'](process.argv.slice(2));
 }
